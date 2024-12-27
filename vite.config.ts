3b70e5de521c6c5c7c2195a5fd733a8340114397
replydev <commoncargo@tutanota.com>
2024-12-27 19:30:09 +0100

    chore: export library types

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
   build: {
-       lib: {
-          entry: resolve(__dirname, 'src/index.ts'),
-          formats: ['es']
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['es']
+      },
+      rollupOptions: {
+        // This is a library and we want to exclude React from the bundle since it will be a peer dependency
+        external: ['react', 'react/jsx-runtime'],
       }
-  }
+  },
 });

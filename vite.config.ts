5da3e09ddaa510b8e70f55d29dd0e03d8877a515
replydev <commoncargo@tutanota.com>
2024-12-27 19:30:08 +0100

    chore: configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+       lib: {
+          entry: resolve(__dirname, 'src/index.ts'),
+          formats: ['es']
+      }
+  }
 });
